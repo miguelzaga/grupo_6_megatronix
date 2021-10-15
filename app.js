6198ca8b9c450cb1c13d90ea217d342732b99f00
Miguel <miguezaga2@hotmail.com>
2021-10-15 09:51:28 -0500

    Agrego página de registro y links de esta al index

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/index.html'))});
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))});
 
 app.listen('3000', () => console.log('Servidor corriendo'));
