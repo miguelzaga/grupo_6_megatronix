6b8eacc6dce128a09aee0262297c6f3aef7a0afc
Javiezapata <jaalzamo@gmail.com>
2021-10-16 17:36:16 -0500

    añadi ruta de acceso en app.js de login y el cuerpo html de login

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/register.html'))});
 
+app.get('/login', (req, res) => {
+     res.sendFile(path.resolve(__dirname,'./views/login.html'))});
+    
+
 app.listen('3000', () => console.log('Servidor corriendo'));
