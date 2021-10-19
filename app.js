4c1f562741389c4e41c36a813bbf514bac74fad4
harolPreciado <harol.andres1999@gmail.com>
2021-10-19 12:22:30 -0500

    Modifiqué header, agregué enlaces a index, inicio  y carrito

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/index.html'))});
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+});
+
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/register.html'))});
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
 
 app.get('/login', (req, res) => {
-     res.sendFile(path.resolve(__dirname,'./views/login.html'))});
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
 
 app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))});
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+});
 
 app.listen('3000', () => console.log('Servidor corriendo'));
