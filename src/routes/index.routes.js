e88a51c91f22a947b3201b9a34cf3e8edebeeb61
Miguel <miguezaga2@hotmail.com>
2021-11-02 17:48:22 -0500

    añado nodemon y ordeno carpetas modo mvc

---
+++ src/routes/index.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+router.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+
+router.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+});
+
+
+module.exports = router;
