c51e7e98597c99e5fa101483270912bfb6f9e534
Miguel <miguezaga2@hotmail.com>
2021-11-18 16:16:26 -0500

    agregar urlencoded y json

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index.routes')
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const { urlencoded } = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+app.use(express.urlencoded({extended:"false"}));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(methodOverride('_method'))
+
 
 app.set('views', path.resolve(__dirname, './views'))
 app.set('view engine', 'ejs')
