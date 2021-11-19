0aeb75da4ab1cdda54e3da1b42a74ca57f76b337
Miguel <miguezaga2@hotmail.com>
2021-11-19 10:36:50 -0500

    modularizo categorias en categorias.json

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const productsdb = require('../model/products.json')
 const usersdb = require('../model/users.json')
+const productsCategories = require('../model/categorias.json')
 
 const controller = {
     index: (req, res) => {
@@ -17,36 +18,40 @@ const controller = {
     },
     // Vista listado de productos
     products: (req, res) => {
-        res.render('products', {products : productsdb});
+        res.render('products',
+            {
+                products: productsdb,
+                categories: productsCategories
+            });
     },
     // Vista formulario de creación de productos
-    create:(req, res) => {
+    create: (req, res) => {
         res.render('createProduct');
     },
     // Creación de un nuevo producto
-    store:(req, res) => {
+    store: (req, res) => {
         // lógica de creado
-        res.send(req.body);// test, pero no esta saliendo nada en el req.body
+        res.send(req.body);
         // res.redirect('/');
     },
     // Vista detalle de un producto particular
     productDetail: (req, res) => {
         let id = req.params.id
         let product = productsdb.find(product => product.id == id)
-        res.render('productDetail', {product: product});
+        res.render('productDetail', { product: product });
     },
-    edit:(req, res) =>{
+    edit: (req, res) => {
         let id = req.params.id
         let product = productsdb.find(product => product.id == id)
-        res.render('editProduct', {product: product});
+        res.render('editProduct', { product: product });
     },
-    update:(req, res) =>{
+    update: (req, res) => {
         let id = req.params.id
         // lógica para editar
-        res.send(req.body);// test, pero no esta saliendo nada en el req.body
+        res.send(req.body);
         // res.redirect('/');
     },
-    destroy:(req, res) =>{
+    destroy: (req, res) => {
         let id = req.params.id
         // lógica para borrar producto
         res.redirect('/')
