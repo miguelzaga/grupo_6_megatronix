7211dba253326a82d8cbcd8db8a80e39cada9cee
Miguel <miguezaga2@hotmail.com>
2021-12-18 20:14:19 -0500

    express-session

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
-const app = express();
+const session = require('express-session')
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended:"false"}));
+app.use(express.urlencoded({ extended: "false" }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(methodOverride('_method'))
-
+app.use(session({
+    secret: "el mensaje secreto de Megatronix",
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.set('views', path.resolve(__dirname, './views'))
 app.set('view engine', 'ejs')
 
- // Routes
+// Routes
 const routes = require('./routes/index.routes')
 const userRoutes = require('./routes/users.routes');
 const productRoutes = require('./routes/products.routes');
@@ -22,6 +27,6 @@ app.use('/', routes);
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 })
--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { validationResult } = require('express-validator')
 const usersPath = path.join(__dirname, '../model/users.json')
 const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const newId = () => {
     let greater = 0;
@@ -26,36 +26,31 @@ const controller = {
     },
 
     // Captura de datos
-    processRegister : (req, res) =>{
+    processRegister: (req, res) => {
         const resultValidation = validationResult(req);
 
         if (resultValidation.errors.length > 0) {
-			return res.render('users/register', {
-				errors: resultValidation.mapped(),
-				oldData: req.body
-			});
-		};
+            return res.render('users/register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        };
 
         let id = newId();
         let file = req.file;
-        let passEncriptada = bcrypt.hashSync(req.body.password, 10); 
-        let newUser = { }
+        let passEncriptada = bcrypt.hashSync(req.body.password, 10);
+        let newUser = {
+                id: id,
+                ...req.body,
+                image: 'perfil.png'
+            }
 
-      
-        if (!file) {
-            newUser ={
-            id: id,
-            ...req.body,
-            image : 'perfil.png'
+        newUser.password = passEncriptada;
+
+        if(file) {
+            newUser.image =  req.file.filename
         }
-        }
-        else {
-            newUser ={
-            id: id,
-            ...req.body,
-            image : req.file.filename}
-        }
-        newUser.password=passEncriptada;
+        
         users.push(newUser);
         let modifiedUsers = JSON.stringify(users, null, 4);
         fs.writeFileSync(usersPath, modifiedUsers)
