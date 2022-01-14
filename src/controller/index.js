d3ef1d0a9c033f60df5d736301a920d181a9d8cd
harolPreciado <harol.andres1999@gmail.com>
2022-01-13 19:40:05 -0500

    Agregue index a controller

---
+++ src/controller/index.js
@@ -0,0 +1,9 @@
+const indexController = require('./index.controller');
+const productController = require('./productController');
+const usersController = require('./usersController');
+
+module.exports = {
+    indexController,
+    productController,
+    usersController
+}
