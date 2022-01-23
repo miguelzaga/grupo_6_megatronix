d53644adcb2878a42d9588d2babe3f1e90388f21
Miguel <miguezaga2@hotmail.com>
2022-01-23 13:35:25 -0500

    crud listado productos

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -4,6 +4,8 @@ const productsPath = path.join(__dirname, '../model/products.json')
 const products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
 const productsCategories = require('../model/categorias.json');
 
+const db = require('../database/models')
+
 const newId = () => {
     let greater = 0;
     products.forEach(product => {
@@ -15,81 +17,105 @@ const newId = () => {
     return greater
 }
 
+let promesaDestacados =
+    db.Product.findAll({
+        include: {
+            model: db.ProductPromotion,
+            as: "ProductPromotions",
+            where: {
+                promotion: "destacado"
+            }
+        },
+        limit: 4
+    })
+
 const productController = {
-        products: (req, res) => {
-            let destacados = products.filter(product => product.category_sales == "destacado")
-            res.render('products/products', { products: products, destacados: destacados, categories: productsCategories });
-        },
-        productCart: (req, res) => {
-            res.render('products/productCart');
-        },
-        create: (req, res) => {
-            res.render('products/createProduct', { categories: productsCategories });
-        },
-        store: (req, res) => {
-            let id = newId();
-            let file = req.file;
-            let newProduct = {};
-    
-            if (!file) {
-                newProduct ={
-                id: id,
-                ...req.body,
-                image : 'default.png'
-            }
-            }
-            else {
-                newProduct ={
-                id: id,
-                ...req.body,
-                image : req.file.filename}
-            }
-    
-            products.push(newProduct);
-            let modifiedProducts = JSON.stringify(products, null, 4);
-            fs.writeFileSync(productsPath, modifiedProducts)
-            res.redirect('/products/' + id);
-        },
-        productDetail: (req, res) => {
-            let id = req.params.id
-            let product = products.find(product => product.id == id)
-            let destacados = products.filter(product => product.category_sales == "destacado")
-            res.render('products/productDetail', { product: product, destacados: destacados });
-        },
-        edit: (req, res) => {
-            let id = req.params.id
-            let product = products.find(product => product.id == id)
-            res.render('products/editProduct', {
-                product: product, categories: productsCategories
-            });
-        },
-        update: (req, res) => {
-            let id = req.params.id
-            let file = req.file;
-    
-            products.map(function(product){
-                if(product.id == id){
-                    Object.keys(req.body).forEach(key => product[key] = req.body[key])
-                    if (file){
-                        product.image = file.filename
-                    }
-                }
-                return product
+    products: (req, res, next) => {
+        let promesaProductos = db.Product.findAll()
+        let promesaCategorias = db.ProductCategory.findAll()
+
+        Promise.all([promesaProductos, promesaCategorias, promesaDestacados])
+            .then(function (arr) {
+                console.log(arr[1])
+                res.render('products/products', { 
+                    products: arr[0],
+                    categories: arr[1],
+                    destacados: arr[2]
+                 });
             })
-    
-            let modifiedProducts = JSON.stringify(products, null, 4);
-            fs.writeFileSync(productsPath, modifiedProducts)
-            res.redirect('/products/' + id);
-            
-        },
-        destroy: (req, res) => {
-            let id = req.params.id
-            let filteredProducts = products.filter(product => product.id != id);
-                
-            let modifiedProducts = JSON.stringify(filteredProducts, null, 4);
-            fs.writeFileSync(productsPath, modifiedProducts)
-            res.redirect('/')
+            .catch(error => console.log(error))
+    },
+    productCart: (req, res) => {
+        res.render('products/productCart');
+    },
+    create: (req, res) => {
+        res.render('products/createProduct', { categories: productsCategories });
+    },
+    store: (req, res) => {
+        let id = newId();
+        let file = req.file;
+        let newProduct = {};
+
+        if (!file) {
+            newProduct = {
+                id: id,
+                ...req.body,
+                image: 'default.png'
+            }
         }
+        else {
+            newProduct = {
+                id: id,
+                ...req.body,
+                image: req.file.filename
+            }
+        }
+
+        products.push(newProduct);
+        let modifiedProducts = JSON.stringify(products, null, 4);
+        fs.writeFileSync(productsPath, modifiedProducts)
+        res.redirect('/products/' + id);
+    },
+    productDetail: (req, res) => {
+        let id = req.params.id
+        let product = products.find(product => product.id == id)
+        let destacados = products.filter(product => product.category_sales == "destacado")
+        res.render('products/productDetail', { product: product, destacados: destacados });
+    },
+    edit: (req, res) => {
+        let id = req.params.id
+        let product = products.find(product => product.id == id)
+        res.render('products/editProduct', {
+            product: product, categories: productsCategories
+        });
+    },
+    update: (req, res) => {
+        let id = req.params.id
+        let file = req.file;
+
+        products.map(function (product) {
+            if (product.id == id) {
+                Object.keys(req.body).forEach(key => product[key] = req.body[key])
+                if (file) {
+                    product.image = file.filename
+                }
+            }
+            return product
+        })
+
+        let modifiedProducts = JSON.stringify(products, null, 4);
+        fs.writeFileSync(productsPath, modifiedProducts)
+        res.redirect('/products/' + id);
+
+    },
+    destroy: (req, res) => {
+        let id = req.params.id
+        let filteredProducts = products.filter(product => product.id != id);
+
+        let modifiedProducts = JSON.stringify(filteredProducts, null, 4);
+        fs.writeFileSync(productsPath, modifiedProducts)
+        res.redirect('/')
+    }
 
 }
 /*
