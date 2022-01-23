8beb4b3541f0774a1629a193a0a669e4da1f11eb
Miguel <miguezaga2@hotmail.com>
2022-01-23 13:17:27 -0500

    crud de index

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,3 +1,4 @@
+const { promiseImpl } = require('ejs');
 const fs = require('fs');
 const path = require('path');
 const productsPath = path.join(__dirname, '../model/products.json')
@@ -6,9 +7,36 @@ const db = require('../database/models');
 
 const indexController = {
     index: (req, res) => {
-        let destacados = products.filter(product => product.category_sales == "destacado")
-        let ofertas = products.filter(product => product.category_sales == "oferta")
-        res.render('index', {destacados: destacados, ofertas: ofertas});
+
+        let promesaDestacados =
+            db.Product.findAll({
+                include: {
+                    model: db.ProductPromotion,
+                    as: "ProductPromotions",
+                    where: {
+                        promotion: "destacado"
+                    }
+                },
+                limit: 4
+            })
+            
+            let promesaOfertas =
+            db.Product.findAll({
+                include: {
+                    model: db.ProductPromotion,
+                    as: "ProductPromotions",
+                    where: {
+                        promotion: "oferta"
+                    }
+                },
+                limit: 4
+            })
+
+        Promise.all([promesaDestacados, promesaOfertas])
+            .then(arr => {
+                res.render('index', { destacados: arr[0], ofertas: arr[1] })
+            })
+            .catch(error => {console.log(error)})
     }
 }
 
