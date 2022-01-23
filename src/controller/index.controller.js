5f345aa9afc53a114a57da302841c515d7898bc9
Miguel <miguezaga2@hotmail.com>
2022-01-23 13:19:02 -0500

    limpiar codigo

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,8 +1,3 @@
-const { promiseImpl } = require('ejs');
-const fs = require('fs');
-const path = require('path');
-const productsPath = path.join(__dirname, '../model/products.json')
-const products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
 const db = require('../database/models');
 
 const indexController = {
