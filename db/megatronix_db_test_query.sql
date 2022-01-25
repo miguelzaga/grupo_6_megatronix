01bdb7af2cdfa482efd401d144542344cede54c3
Miguel <miguezaga2@hotmail.com>
2022-01-24 20:55:11 -0500

    actualizo test query

--- db/megatronix_db_test_query.sql
+++ db/megatronix_db_test_query.sql
@@ -3,8 +3,7 @@ use megatronix_db;
 select u.email email, ucat.category user, p.name product, pc.category category, pp.promotion promotion
 from Users u
 join UserCategories ucat on u.user_category_id = ucat.id 
-join UserCarts uc on u.id = uc.id
-join ProductsInCarts pic on uc.id = pic.user_cart_id
+join ProductsInCarts pic on u.id = pic.user_id
 join Products p on pic.product_id = p.id
 join ProductCategories pc on p.product_category_id = pc.id
 join ProductPromotions pp on p.product_promotion_id = pp.id;
