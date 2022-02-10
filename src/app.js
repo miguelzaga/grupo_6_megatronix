a4cadbf324d470aeeaef72c1d6d3d88ee7aae2fc
Javier Zapata <jaalzamo@gmail.com>
2022-02-10 01:17:24 -0500

    entorno prueba 2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // require('dotenv').config() --> Falta instalar dotenv
 const app= require('./app');
-require('dotenv').config()
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
